e733b42505db6496a5cd4e6d82e536f529b95f51
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-25 19:26:42 +0000

    Vanilla JavaScript Function, Arrow Function With Parameters, Single Line Arrow Function With Parameters, Implicit Returns, Returning Multiple Lines

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,44 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers1(a, b) {
+    return a + b;
+}
+
+let sum1 = addTwoNumbers1(3, 5);
+console.log(sum1);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers2 = (a, b) => {
+    return a + b;
+}
+
+let sum2 = addTwoNumbers2(3, 5);
+console.log(sum2);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers3 = (a, b) => a + b;
+
+let sum3 = addTwoNumbers3(3, 5);
+console.log(sum3);
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('This is a message');
+
+const sayHello = () => console.log('Hello');
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `
+<p>
+This is a multiline string!
+</p>
+    `
+    )
+console.log(returnMultipleLines());
